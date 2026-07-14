905bf82ec5a62a82cfe40d150e069439a3ffe982
maheshannavaram92-arch <maheshannavaram92@gmail.com>
2026-07-14 15:41:48 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "zone-id" {
+        default = "Z0589038125I1IDMNOB0D"
+        }
+ 
+variable "blue-ip" {
+        default = "10.10.10.25"
+        }
+ 
+variable "green-ip" {
+        default = "10.10.10.21"
+        }
